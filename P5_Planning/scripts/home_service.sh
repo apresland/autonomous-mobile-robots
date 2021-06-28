fd67bd5982f02957589a1964bd9fff8776885fb2
andrew presland <andrew.presland@gmail.com>
2021-06-28 02:48:02 -0700

    feat: Configure planning demo

--- P5_Planning/scripts/home_service.sh
+++ P5_Planning/scripts/home_service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 xterm  -e "
-roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/simple.world " &
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find add_markers)/config/simple.world " &
 sleep 5
 
 xterm  -e "
-roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/maps/simple.yaml" &
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find add_markers)/config/simple.yaml" &
 sleep 5
 
 xterm  -e "
